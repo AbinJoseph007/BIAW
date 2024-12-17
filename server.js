1c1c909d7ff07b1ecc88129d9d3d8e7a6b53e637
AbinJoseph007 <abinjoseph12303@gmail.com>
2024-12-18 04:27:46 +0530

    create checkout

--- server.js
+++ server.js
@@ -14,32 +14,34 @@ app.use(cors());
 
 app.use(express.json());
 
-// Endpoint to create a checkout session
-app.post('/create-checkout-session', async (req, res) => {
-    try {
-        const { seats, clientReferenceId, priceId } = req.body;
+app.get('/create-checkout-session', (req, res) => { res.status(200).send('Test: Hello World!'); });
 
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types: ['card'],
-            line_items: [
-                {
-                    price: priceId,
-                    quantity: seats,
-                },
-            ],
-            // mode: 'payment',
-            // client_reference_id: clientReferenceId,
-            // success_url: 'https://new.biaw.com/thank-you',
-            // cancel_url: 'https://new.biaw.com/payment-declined',
-            // allow_promotion_codes: true, // Enable promo codes
-        });
+// // Endpoint to create a checkout session
+// app.post('/create-checkout-session', async (req, res) => {
+//     try {
+//         const { seats, clientReferenceId, priceId } = req.body;
 
-        res.json({ url: session.url });
-    } catch (error) {
-        console.error('Error creating checkout session:', error.message);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
+//         const session = await stripe.checkout.sessions.create({
+//             payment_method_types: ['card'],
+//             line_items: [
+//                 {
+//                     price: priceId,
+//                     quantity: seats,
+//                 },
+//             ],
+//             mode: 'payment',
+//             client_reference_id: clientReferenceId,
+//             success_url: 'https://new.biaw.com/thank-you',
+//             cancel_url: 'https://new.biaw.com/payment-declined',
+//             allow_promotion_codes: true, // Enable promo codes
+//         });
+
+//         res.json({ url: session.url });
+//     } catch (error) {
+//         console.error('Error creating checkout session:', error.message);
+//         res.status(500).json({ error: 'Internal Server Error' });
+//     }
+// });
 
 // Start server
 app.listen(PORT, () => {
